646bcf5bf570dd3f113812d9a7ecb283a846a7dc
frellar <frellar@hotmail.com>
2023-06-30 17:00:02 -0400

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach <br> & Rocket Smoothie')
+streamlit.text('Kale, Spinach \n & Rocket Smoothie')
 streamlit.header('Hard-Boiled Free-Range Egg')
 
